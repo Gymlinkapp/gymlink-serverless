1e7f236d4af96b9cb050791ac5390a0e9489ca2c
Dylan <dylan.reeder5@gmail.com>
2023-04-13 21:07:53 -0400

    handled sign in properly

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -4,8 +4,10 @@ import jwt from 'jsonwebtoken';
 import prisma from '@/lib/prisma';
 
 type Data = {
-  email: string;
-  password: string;
+  email?: string;
+  password?: string;
+  token?: string;
+  error?: string;
 };
 
 export default async function handler(
@@ -19,31 +21,36 @@ export default async function handler(
 
   const input: Input = req.body;
 
-  console.log('input', input);
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
+    });
+    if (!user) throw new Error('User not found');
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: input.email,
-    },
-  });
-  if (!user) throw new Error('User not found');
+    //   const passwordsMatch = await bcrypt.compare(input.password, user.password);
+    const passwordsMatch = input.password === user.password;
 
-  //   const passwordsMatch = await bcrypt.compare(input.password, user.password);
-  const passwordsMatch = input.password === user.password;
+    if (!passwordsMatch) throw new Error('Incorrect password');
 
-  if (!passwordsMatch) throw new Error('Incorrect password');
+    const token = jwt.sign(
+      { email: user.email },
+      process.env.JWT_SECRET as string
+    );
 
-  const token = jwt.sign(
-    { email: user.email },
-    process.env.JWT_SECRET as string
-  );
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        tempJWT: token,
+      },
+    });
 
-  await prisma.user.update({
-    where: {
-      id: user.id,
-    },
-    data: {
-      tempJWT: token,
-    },
-  });
+    res.status(200).json({ email: user.email, password: user.password, token });
+  } catch (error) {
+    console.log('error');
+    res.status(500).json({ error: 'error' });
+  }
 }
