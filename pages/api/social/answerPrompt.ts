08354b1f43324434df5c2ff90339563d9f92463e
Dylan <dylan.reeder5@gmail.com>
2023-04-22 10:43:26 -0400

    endpoint for a user to answer a prompt

---
+++ pages/api/social/answerPrompt.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import prisma from '@/lib/prisma';
+import { GenericData } from '@/types/GenericData';
+
+type Data = {} & GenericData;
+
+type Input = {
+  userId: string;
+  promptId: string;
+  answer: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const input = req.body as Input;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: input.userId,
+      },
+    });
+
+    if (!user) throw new Error('User not found');
+
+    const prompt = await prisma.prompt.findUnique({
+      where: {
+        id: input.promptId,
+      },
+    });
+
+    if (!prompt) throw new Error('Prompt not found');
+
+    const userPrompt = await prisma.userPrompt.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        prompt: {
+          connect: {
+            id: prompt.id,
+          },
+        },
+        answer: input.answer,
+        hasAnswered: true,
+      },
+    });
+
+    return res.status(200).json({ userPrompt });
+  } catch (error) {
+    console.log(error);
+    throw new Error('error');
+  }
+}
