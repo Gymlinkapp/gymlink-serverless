1915c322ba3cd84b97d56a30d8d5dad3daf2c9b9
Dylan <dylan.reeder5@gmail.com>
2023-04-22 14:48:43 -0400

    temp: create userPrompt when generating prompt

--- pages/api/social/generatePrompt.ts
+++ pages/api/social/generatePrompt.ts
@@ -17,27 +17,47 @@ export default async function handler(
   };
 
   try {
-    const result = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.OPENAI_API_TOKEN}`,
-      },
-      body: JSON.stringify({
-        messages: [gptPrompt],
-        model: 'gpt-3.5-turbo',
-        max_tokens: 25,
-      }),
-    });
+    // const result = await fetch('https://api.openai.com/v1/chat/completions', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Authorization: `Bearer ${process.env.OPENAI_API_TOKEN}`,
+    //   },
+    //   body: JSON.stringify({
+    //     messages: [gptPrompt],
+    //     model: 'gpt-3.5-turbo',
+    //     max_tokens: 25,
+    //   }),
+    // });
 
-    const data = await result.json();
-    const prompt = data.choices[0].message.content;
-    await prisma.prompt.create({
+    // const data = await result.json();
+    // const prompt = data.choices[0].message.content;
+    const users = await prisma.user.findMany({
+      where: {
+        isBot: false,
+      },
+    });
+    const prompt = "What's your go-to music for the gym right now?";
+    const createdPrompt = await prisma.prompt.create({
       data: {
         prompt,
       },
     });
-    return res.status(200).json({ data, prompt });
+
+    await Promise.all(
+      users.map((user: User) =>
+        prisma.userPrompt.create({
+          data: {
+            userId: user.id,
+            promptId: createdPrompt.id,
+            hasAnswered: false,
+            answer: '',
+          },
+        })
+      )
+    );
+    // return res.status(200).json({ data, prompt });
+    return res.status(200).json({ prompt });
   } catch (error) {
     console.log(error);
     throw new Error('error');
