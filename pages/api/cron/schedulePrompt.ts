fe1c4e0c3e8169a0e3aac07708ab2df290f9a17a
Dylan <dylan.reeder5@gmail.com>
2023-04-26 13:43:25 -0400

    optimize schedule prompt

--- pages/api/cron/schedulePrompt.ts
+++ pages/api/cron/schedulePrompt.ts
@@ -9,38 +9,43 @@ type Input = {
   token: string;
 };
 
+const gptPrompt = {
+  role: 'assistant',
+  content:
+    'create one random prompt for a gym goer to answer to express their mood/vibe/what their doing. This prompt will allow users to quickly find connections by similarities? This could include questions to gain insight about their gym goals, what they are doing at the gym, or what they are doing after the gym, music they are listening to before, after, during their workouts. etc. I want to aim for a new and random prompt everyday. The question/prompt should be short and sweet and a good way to start a conversation and connect with someone.',
+};
+
+async function fetchPrompt() {
+  const result = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.OPENAI_API_TOKEN}`,
+    },
+    body: JSON.stringify({
+      messages: [gptPrompt],
+      model: 'gpt-3.5-turbo',
+      max_tokens: 25,
+    }),
+  });
+  const data = await result.json();
+  return data.choices[0].message.content;
+}
+
+async function fetchUsers() {
+  return await prisma.user.findMany({
+    where: {
+      isBot: false,
+    },
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const gptPrompt = {
-    role: 'assistant',
-    content:
-      'create one random prompt for a gym goer to answer to express their mood/vibe/what their doing. This prompt will allow users to quickly find connections by similarities? This could include questions to gain insight about their gym goals, what they are doing at the gym, or what they are doing after the gym, music they are listening to before, after, during their workouts. etc. I want to aim for a new and random prompt everyday. The question/prompt should be short and sweet and a good way to start a conversation and connect with someone.',
-  };
-
   try {
-    console.log('Fetching prompt from OpenAI');
-    const result = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.OPENAI_API_TOKEN}`,
-      },
-      body: JSON.stringify({
-        messages: [gptPrompt],
-        model: 'gpt-3.5-turbo',
-        max_tokens: 25,
-      }),
-    });
-
-    const data = await result.json();
-    const prompt = data.choices[0].message.content;
-    const users = await prisma.user.findMany({
-      where: {
-        isBot: false,
-      },
-    });
+    const [prompt, users] = await Promise.all([fetchPrompt(), fetchUsers()]);
     // const prompt = "What's your gym goal this week?"; // test
     console.log('Creating prompt in database');
     const createdPrompt = await prisma.prompt.create({
@@ -79,7 +84,7 @@ export default async function handler(
       body: JSON.stringify(message),
     });
     console.log('Push notification sent');
-    return res.status(200).json({ data, prompt });
+    return res.status(200).json({ prompt });
     // return res.status(200).json({ prompt }); // test
   } catch (error) {
     console.log(error);
