8f70dc8172419b388d500954440c83532ba4e7fa
Dylan <dylan.reeder5@gmail.com>
2023-05-07 14:58:29 -0400

    update create post to use email from user

--- pages/api/posts/create.ts
+++ pages/api/posts/create.ts
@@ -10,6 +10,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Handle the OPTIONS request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   try {
     const input = req.body as Input;
 
