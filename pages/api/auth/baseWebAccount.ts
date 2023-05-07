cf7f969e6dd7fabf49e63e830a57b5af9b8f5712
Dylan <dylan.reeder5@gmail.com>
2023-05-06 22:40:42 -0400

    this is the fix for the base web account creation

--- pages/api/auth/baseWebAccount.ts
+++ pages/api/auth/baseWebAccount.ts
@@ -18,8 +18,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Handle the OPTIONS request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   const input = req.body as Input;
-  res.setHeader("Access-Control-Allow-Origin", "localhost:3000");
   if (
     input.baseWebAccount &&
     input.email &&
