bbeaacddff2965cb4e218565b8403c47451b2271
Dylan <dylan.reeder5@gmail.com>
2023-05-08 10:50:47 -0400

    add header options to resource endpoint

--- pages/api/dashboard/resources.ts
+++ pages/api/dashboard/resources.ts
@@ -15,8 +15,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'https://dashboard.gymlink.app');
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Handle the OPTIONS request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   try {
     const users = await prisma.user.findMany();
 
