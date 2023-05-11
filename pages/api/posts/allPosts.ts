2d5d653947f77c3e408424fd1447e536e7c7f8d6
Dylan <dylan.reeder5@gmail.com>
2023-05-10 20:43:50 -0400

    fix cors with getting all posts

--- pages/api/posts/allPosts.ts
+++ pages/api/posts/allPosts.ts
@@ -1,26 +1,45 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Handle the OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   const posts = await prisma.post.findMany({
     orderBy: {
-      createdAt: 'desc'
+      createdAt: 'desc',
     },
     include: {
       user: true,
       likes: true,
       comments: {
         include: {
-          user: true
+          user: true,
         },
         orderBy: {
-          createdAt: 'desc'
+          createdAt: 'desc',
         },
       },
-    }
-  })
+    },
+  });
 
-  res.status(200).json(posts)
-
+  res.status(200).json(posts);
 }
